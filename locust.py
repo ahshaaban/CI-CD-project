fecd59378780a33211e08e986b13182f7cbaf697
Ahmed Shaaban <95375151+ahshaaban@users.noreply.github.com>
2022-12-04 12:43:34 +0200

    Update locust.py

--- locust.py
+++ locust.py
@@ -1,12 +1,7 @@
-import time
-from locust import HttpUser, task, between
+from locust import HttpUser, task
 
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 2.5)
-
-    @task(1)
-    def test1(self):
-        self.client.get("https://flask-ci-cd.azurewebsites.net")
-     @task(2)
-    def test2(self):
-        self.client.post("https://flask-ci-cd.azurewebsites.net:443/predict")
+class HelloWorldUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("/hello")
+        self.client.get("/world")
